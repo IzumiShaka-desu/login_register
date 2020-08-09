8e90269627e1e495c490182505abee501d1c318c
sesakaaji19137 <sesaka.aji19137@student.unsika.ac.id>
2020-08-09 15:47:15 +0700

    completing login act

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,10 +1,27 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
-loginAction(BuildContext context,String username,String password) {
-  if(username.trim()=="admin" && password =="admin"){
-      
+SnackBar buildSnackBar(String message,Color color,IconData icon) => SnackBar(
+        content: Container(
+      child: Center(
+        child: Row(
+          children: [
+            Icon(icon,color: color),
+            SizedBox(width:10),
+            Text(message,style: TextStyle(color: color),)
+          ],
+        ),
+      ),
+    ));
+
+loginAction(BuildContext context, GlobalKey<ScaffoldState> key, String username,
+    String password) {
+  if (username.trim() == "admin" && password == "admin") {
+    key.currentState.showSnackBar(buildSnackBar('login Succesfull',Colors.greenAccent[700],Icons.check));
+    Navigator.of(context).push(MaterialPageRoute(builder: (context)=>DashBoard({username:username,password:password})));
+  }else{
+    key.currentState.showSnackBar(buildSnackBar('username or password wrong ,please try again',Colors.orange[700],Icons.warning));
   }
-}
-signUpAction(){
 
 }
+
+signUpAction() {}
