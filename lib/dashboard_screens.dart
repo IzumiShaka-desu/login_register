659d811ece395ed10ef6eed6e7ab1cd704701f05
sesakaaji19137 <sesaka.aji19137@student.unsika.ac.id>
2020-08-09 16:21:19 +0700

    oke

--- lib/dashboard_screens.dart
+++ lib/dashboard_screens.dart
@@ -5,47 +5,94 @@ class DashBoard extends StatelessWidget {
   final String username, password;
   @override
   Widget build(BuildContext context) {
+    
+    double height = MediaQuery.of(context).size.height;
     return Scaffold(
         body: SafeArea(
       child: Container(
+          padding: EdgeInsets.only(left:10),
           child: Column(
-        children: [
-          Row(
             children: [
-              Container(
-                child: Row(
-                  children: [
-                    CircleAvatar(
-                      child: Icon(Icons.supervised_user_circle),
-                    ),
-                    SizedBox(width: 10),
-                    Text('Selamat datang $username')
-                  ],
-                ),
-              ),
-              IconButton(
-                  icon: Icon(Icons.exit_to_app),
-                  onPressed: () => Navigator.of(context).canPop())
-            ],
-          ),
-          Center(
-            child: Card(
-              margin: EdgeInsets.all(20),
-              child: Container(
-                  padding: EdgeInsets.all(20),
-                  child: Column(
+              SizedBox(
+                width: double.infinity,
+                height: height / 7,
+                child:Stack(children: [
+                Positioned(
+                    top: -8,
+                    right: -8,
+                    child: Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                          color: Colors.greenAccent, shape: BoxShape.circle),
+                    )),
+                Positioned(
+                    top: 55,
+                    right: 40,
+                    child: Container(
+                      height: 30,
+                      width: 30,
+                      decoration: BoxDecoration(
+                          color: Colors.blue, shape: BoxShape.circle),
+                    )),
+                Positioned(
+                    top: 60,
+                    right: 90,
+                    child: Container(
+                      height: 10,
+                      width: 10,
+                      decoration: BoxDecoration(
+                          color: Colors.pink, shape: BoxShape.circle),
+                    )),
+                Positioned(
+                    top: 20,
+                    right: 100,
+                    child: Container(
+                      height: 20,
+                      width: 20,
+                      decoration: BoxDecoration(
+                          color: Colors.orange, shape: BoxShape.circle),
+                    )),
+                Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text('Username : $username'),
-                      SizedBox(
-                        height: 10,
+                      Container(
+                        child: Row(
+                          children: [
+                            CircleAvatar(
+                              child: Icon(Icons.supervised_user_circle),
+                            ),
+                            SizedBox(width: 10),
+                            Text('Selamat datang $username')
+                          ],
+                        ),
                       ),
-                      Text('password : $password')
+                      IconButton(
+                          icon: Icon(Icons.exit_to_app),
+                          onPressed: () => Navigator.of(context).pop())
                     ],
-                  )),
-            ),
-          )
-        ],
-      )),
+                  ),
+                ),
+              ])),
+              Center(
+                child: Card(
+                  margin: EdgeInsets.all(20),
+                  child: Container(
+                      padding: EdgeInsets.all(20),
+                      child: Column(
+                        children: [
+                          Text('Username : $username'),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text('password : $password')
+                        ],
+                      )),
+                ),
+              )
+            ],
+          )),
     ));
   }
 }
--- lib/utils.dart
+++ lib/utils.dart
@@ -4,12 +4,14 @@ import 'dashboard_screens.dart';
 
 SnackBar buildSnackBar(String message,Color color,IconData icon) => SnackBar(
         content: Container(
+          height: 40,
       child: Center(
+        
         child: Row(
           children: [
             Icon(icon,color: color),
             SizedBox(width:10),
-            Text(message,style: TextStyle(color: color),)
+            Expanded(child: Text(message,style: TextStyle(color: color),))
           ],
         ),
       ),
@@ -18,7 +20,7 @@ SnackBar buildSnackBar(String message,Color color,IconData icon) => SnackBar(
 loginAction(BuildContext context, GlobalKey<ScaffoldState> key, String username,
     String password) {
   if (username.trim() == "admin" && password == "admin") {
-    key.currentState.showSnackBar(buildSnackBar('login Succesfull',Colors.greenAccent[700],Icons.check));
+    //key.currentState.showSnackBar(buildSnackBar('login Succesfull',Colors.greenAccent[700],Icons.check));
     Navigator.of(context).push(MaterialPageRoute(builder: (context)=>DashBoard(username:username,password:password)));
   }else{
     key.currentState.showSnackBar(buildSnackBar('username or password wrong ,please try again',Colors.orange[700],Icons.warning));
